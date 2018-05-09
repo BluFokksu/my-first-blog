ad2d5723faa439974e3aa99da7417d166a8f9c0c
BluFokksu <39114971+BluFokksu@users.noreply.github.com>
2018-05-09 04:15:20 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$i*#!xr8s(_q&@ql)ze0=vpi6$fnthec+5zxp3t9rs=&kpyc)j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'BluFokksu.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'blufokksu.pythonanywhere.com']
 
 
 # Application definition
